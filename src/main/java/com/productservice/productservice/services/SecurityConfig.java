09a1c2f9cec87c3902d26b09df8ad10102593826
NIKHIL <nikhil9nayak@gmail.com>
2024-05-13 21:40:46 +0530

    Day-218: Integration betn US and PS.

---
+++ src/main/java/com/productservice/productservice/services/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.productservice.productservice.services;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
