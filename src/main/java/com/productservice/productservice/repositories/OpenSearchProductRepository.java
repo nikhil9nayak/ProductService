fa66fe0a6f1e9955c84f7bd022c2371ece8accb1
NIKHIL <nikhil9nayak@gmail.com>
2024-05-24 14:37:35 +0530

    Day-229: Error handled for ElasticSearch

--- src/main/java/com/productservice/productservice/repositories/OpenSearchProductRepository.java
+++ src/main/java/com/productservice/productservice/repositories/OpenSearchProductRepository.java
@@ -2,13 +2,14 @@ package com.productservice.productservice.repositories;
 
 import com.productservice.productservice.models.Product;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface OpenSearchProductRepository extends OpenSearchRepository<Product, Long>{
+public interface OpenSearchProductRepository extends ElasticsearchRepository<Product, Long> {
 
     List<Product> findAllByTitle(String title);
     Optional<Product> findById(Long productId);
